6e7fb92d17b76872e54e3146f3618bca68cc44c2
shiraji <isogai.shiraji@gmail.com>
2016-06-20 16:14:04 +0900

    Try gradle project wizard settings

--- src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleBuilder.kt
+++ src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleBuilder.kt
@@ -1,42 +1,56 @@
 package com.github.shiraji.kipw.sample
 
-import com.intellij.ide.projectView.actions.MarkRootActionBase
 import com.intellij.ide.util.projectWizard.JavaModuleBuilder
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.roots.ModifiableRootModel
-import com.intellij.openapi.startup.StartupManager
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VfsUtilCore
-import org.jetbrains.idea.devkit.build.PluginBuildConfiguration
-import org.jetbrains.jps.model.java.JavaResourceRootType
+import java.io.File
 
 class DemoModuleBuilder : JavaModuleBuilder() {
 
+    var rootProjectPath: String? = null
+
     override fun getModuleType() = DemoModuleType.getInstance()
 
-    override fun setupRootModel(rootModel: ModifiableRootModel?) {
-        super.setupRootModel(rootModel)
-        val contentEntryPath = contentEntryPath ?: return
-        val resourceRootPath = "$contentEntryPath/resources"
-        val contentRoot = LocalFileSystem.getInstance().findFileByPath(contentEntryPath) ?: return
-        rootModel ?: return
-        MarkRootActionBase.findContentEntry(rootModel, contentRoot)?.let {
-            it.addSourceFolder(VfsUtilCore.pathToUrl(resourceRootPath), JavaResourceRootType.RESOURCE)
+    override fun setupRootModel(modifiableRootModel: ModifiableRootModel?) {
+        val contentEntryPath = contentEntryPath
+        if (contentEntryPath.isNullOrEmpty()) return
+        val contentRootDir = File(contentEntryPath)
+        FileUtilRt.createDirectory(contentRootDir)
+        val fileSystem = LocalFileSystem.getInstance()
+        val modelContentRootDir = fileSystem.refreshAndFindFileByIoFile(contentRootDir) ?: return
+        modifiableRootModel ?: return
+        modifiableRootModel.addContentEntry(modelContentRootDir);
+        // todo this should be moved to generic ModuleBuilder
+        if (myJdk == null) {
+            modifiableRootModel.inheritSdk();
+        } else {
+            modifiableRootModel.sdk = myJdk;
         }
 
-        val defaultPluginXMLLocation = "$resourceRootPath/META-INF/plugin.xml"
-        val module = rootModel.module
-        val project = module.project
-        StartupManager.getInstance(project).runWhenProjectIsInitialized {
-            PluginBuildConfiguration.getInstance(module)?.let {
-                it.setPluginXmlPathAndCreateDescriptorIfDoesntExist(defaultPluginXMLLocation)
-            }
+        val project = modifiableRootModel.project;
+//        if (myParentProject != null) {
+//            rootProjectPath = myParentProject.getLinkedExternalProjectPath();
+//        } else {
+        rootProjectPath =
+                FileUtil.toCanonicalPath(project.basePath)// : modelContentRootDir.getPath());
+//        }
 
-            LocalFileSystem.getInstance().findFileByPath(defaultPluginXMLLocation)?.let {
-                FileEditorManager.getInstance(project).openFile(it, true)
-            }
+        // FROM HERE
+
+        val gradleBuildFile = setupGradleBuildFile(modelContentRootDir)
+        setupGradleSettingsFile(
+                rootProjectPath, modelContentRootDir, modifiableRootModel.getProject().getName(),
+                myProjectId == null ? modifiableRootModel.getModule().getName() : myProjectId.getArtifactId(),
+        myWizardContext.isCreatingNewProject() || myParentProject == null
+        );
+
+        if (gradleBuildFile != null) {
+            modifiableRootModel.getModule().putUserData(
+                    BUILD_SCRIPT_DATA, new BuildScriptDataBuilder(gradleBuildFile));
         }
     }
 
