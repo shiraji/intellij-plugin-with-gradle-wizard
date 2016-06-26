af96f0bd9dac3fc077e2b0b3e74292d0123d0c3c
shiraji <isogai.shiraji@gmail.com>
2016-06-27 06:59:18 +0900

    Add config to UI

--- src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleConfig.kt
+++ src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleConfig.kt
@@ -3,7 +3,6 @@
 package com.github.shiraji.kipw.sample
 
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.openapi.project.Project
 
 object DemoModuleConfig {
     val PLUGIN_ID_KEY = "com.github.shiraji.foo.pluginid"
@@ -15,21 +14,21 @@ object DemoModuleConfig {
     val GRADLE_PLUGIN_VERSION = "com.github.shiraji.foo.gradlepluginversion"
     val INTELLIJ_VERSION = "com.github.shiraji.foo.intellijversion"
 
-    @JvmStatic fun setPluginId(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(PLUGIN_ID_KEY, value)
-    @JvmStatic fun setPluginName(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(PLUGIN_NAME_KEY, value)
-    @JvmStatic fun setPluginVersion(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(PLUGIN_VERSION_KEY, value)
-    @JvmStatic fun setVendorEmail(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(VENDOR_EMAIL, value)
-    @JvmStatic fun setVendorUrl(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(VENDOR_URL, value)
-    @JvmStatic fun setVendorName(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(VENDOR_NAME, value)
-    @JvmStatic fun setGradlePluginVersion(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(GRADLE_PLUGIN_VERSION, value)
-    @JvmStatic fun setIntellijVersion(project: Project, value: String) = PropertiesComponent.getInstance(project).setValue(INTELLIJ_VERSION, value)
+    @JvmStatic fun setPluginId(value: String) = PropertiesComponent.getInstance().setValue(PLUGIN_ID_KEY, value)
+    @JvmStatic fun setPluginName(value: String) = PropertiesComponent.getInstance().setValue(PLUGIN_NAME_KEY, value)
+    @JvmStatic fun setPluginVersion(value: String) = PropertiesComponent.getInstance().setValue(PLUGIN_VERSION_KEY, value)
+    @JvmStatic fun setVendorEmail(value: String) = PropertiesComponent.getInstance().setValue(VENDOR_EMAIL, value)
+    @JvmStatic fun setVendorUrl(value: String) = PropertiesComponent.getInstance().setValue(VENDOR_URL, value)
+    @JvmStatic fun setVendorName(value: String) = PropertiesComponent.getInstance().setValue(VENDOR_NAME, value)
+    @JvmStatic fun setGradlePluginVersion(value: String) = PropertiesComponent.getInstance().setValue(GRADLE_PLUGIN_VERSION, value)
+    @JvmStatic fun setIntellijVersion(value: String) = PropertiesComponent.getInstance().setValue(INTELLIJ_VERSION, value)
 
-    @JvmStatic fun getPluginId(project: Project) = PropertiesComponent.getInstance(project).getValue(PLUGIN_ID_KEY)
-    @JvmStatic fun getPluginName(project: Project) = PropertiesComponent.getInstance(project).getValue(PLUGIN_NAME_KEY)
-    @JvmStatic fun getPluginVersion(project: Project) = PropertiesComponent.getInstance(project).getValue(PLUGIN_VERSION_KEY)
-    @JvmStatic fun getVendorEmail(project: Project) = PropertiesComponent.getInstance(project).getValue(VENDOR_EMAIL)
-    @JvmStatic fun getVendorUrl(project: Project) = PropertiesComponent.getInstance(project).getValue(VENDOR_URL)
-    @JvmStatic fun getVendorName(project: Project) = PropertiesComponent.getInstance(project).getValue(VENDOR_NAME)
-    @JvmStatic fun getGradlePluginVersion(project: Project) = PropertiesComponent.getInstance(project).getValue(GRADLE_PLUGIN_VERSION)
-    @JvmStatic fun getIntellijVersion(project: Project) = PropertiesComponent.getInstance(project).getValue(INTELLIJ_VERSION)
+    @JvmStatic fun getPluginId() = PropertiesComponent.getInstance().getValue(PLUGIN_ID_KEY)
+    @JvmStatic fun getPluginName() = PropertiesComponent.getInstance().getValue(PLUGIN_NAME_KEY)
+    @JvmStatic fun getPluginVersion() = PropertiesComponent.getInstance().getValue(PLUGIN_VERSION_KEY)
+    @JvmStatic fun getVendorEmail() = PropertiesComponent.getInstance().getValue(VENDOR_EMAIL)
+    @JvmStatic fun getVendorUrl() = PropertiesComponent.getInstance().getValue(VENDOR_URL)
+    @JvmStatic fun getVendorName() = PropertiesComponent.getInstance().getValue(VENDOR_NAME)
+    @JvmStatic fun getGradlePluginVersion() = PropertiesComponent.getInstance().getValue(GRADLE_PLUGIN_VERSION)
+    @JvmStatic fun getIntellijVersion() = PropertiesComponent.getInstance().getValue(INTELLIJ_VERSION)
 }
--- src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleWizardSetup2.java
+++ src/main/kotlin/com/github/shiraji/kipw/sample/DemoModuleWizardSetup2.java
@@ -3,6 +3,7 @@ package com.github.shiraji.kipw.sample;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 
 import javax.swing.*;
@@ -24,6 +25,30 @@ public class DemoModuleWizardSetup2 extends ModuleWizardStep implements Disposab
     public DemoModuleWizardSetup2(WizardContext wizardContext, DemoModuleBuilder builder) {
         this.wizardContext = wizardContext;
         this.builder = builder;
+
+        String text = DemoModuleConfig.getGradlePluginVersion();
+        if (text != null) version.setText(text);
+
+        text = DemoModuleConfig.getIntellijVersion();
+        if (text != null) intellijVersion.setText(text);
+
+        text = DemoModuleConfig.getPluginId();
+        if (text != null) pluginId.setText(text);
+
+        text = DemoModuleConfig.getPluginName();
+        if (text != null) pluginName.setText(text);
+
+        text = DemoModuleConfig.getPluginVersion();
+        if (text != null) pluginVersion.setText(text);
+
+        text = DemoModuleConfig.getVendorEmail();
+        if (text != null) vendorEmail.setText(text);
+
+        text = DemoModuleConfig.getVendorName();
+        if (text != null) vendorName.setText(text);
+
+        text = DemoModuleConfig.getVendorUrl();
+        if (text != null) vendorUrl.setText(text);
     }
 
     @Override
@@ -33,14 +58,39 @@ public class DemoModuleWizardSetup2 extends ModuleWizardStep implements Disposab
 
     @Override
     public void updateDataModel() {
-        builder.setGradlePluginVersion(version.getText());
-        builder.setIntellijVersion(intellijVersion.getText());
-        builder.setPluginName(pluginName.getText());
-        builder.setPluginId(pluginId.getText());
-        builder.setPluginVersion(pluginVersion.getText());
-        builder.setVendorEmail(vendorEmail.getText());
-        builder.setVendorName(vendorName.getText());
-        builder.setVendorUrl(vendorUrl.getText());
+        Project project = wizardContext.getProject();
+
+        String text = version.getText();
+        builder.setGradlePluginVersion(text);
+        DemoModuleConfig.setGradlePluginVersion(text);
+
+        text = intellijVersion.getText();
+        builder.setIntellijVersion(text);
+        DemoModuleConfig.setIntellijVersion(text);
+
+        text = pluginName.getText();
+        builder.setPluginName(text);
+        DemoModuleConfig.setPluginName(text);
+
+        text = pluginId.getText();
+        builder.setPluginId(text);
+        DemoModuleConfig.setPluginId(text);
+
+        text = pluginVersion.getText();
+        builder.setPluginVersion(text);
+        DemoModuleConfig.setPluginVersion(text);
+
+        text = vendorEmail.getText();
+        builder.setVendorEmail(text);
+        DemoModuleConfig.setVendorEmail(text);
+
+        text = vendorName.getText();
+        builder.setVendorName(text);
+        DemoModuleConfig.setVendorName(text);
+
+        text = vendorUrl.getText();
+        builder.setVendorUrl(text);
+        DemoModuleConfig.setVendorUrl(text);
     }
 
     @Override
